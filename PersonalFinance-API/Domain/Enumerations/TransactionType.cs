d8cf627499dcea055d153466b6e4e2c31627489b
jasonmason123 <buiminhson940@gmail.com>
2026-03-28 19:43:38 +0700

    Added string-enum converter to TransactionType

--- PersonalFinance-API/Domain/Enumerations/TransactionType.cs
+++ PersonalFinance-API/Domain/Enumerations/TransactionType.cs
@@ -1,8 +1,11 @@
-﻿namespace PersonalFinance.Domain.Enumerations
+﻿using System.Text.Json.Serialization;
+
+namespace PersonalFinance.Domain.Enumerations
 {
     /// <summary>
     /// Transaction types, with 2 types: <see cref="TransactionType.Income"/> and <see cref="TransactionType.Expense"/>
     /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter<TransactionType>))]
     public enum TransactionType
     {
         /// <summary>
